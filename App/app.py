ba25b9dd08700dc2c9f0037c4c2b7f9cb84dd880
prashant-2309 <singh2309prashant@gmail.com>
2022-12-16 00:43:29 +0530

    Update app.py

--- App/app.py
+++ App/app.py
@@ -18,58 +18,30 @@ from datetime import datetime
 # Utils
 import joblib
 
-
-
-
 # Track Utils
 from track_utils import create_page_visited_table,add_page_visited_details,view_all_page_visited_details,add_prediction_details,view_all_prediction_details,create_emotionclf_table
 
-
-
 #Functions below
-def predict_emotions(docx):
-    pipe_lr = joblib.load(open("/app/end2end-nlp-project/App/models/emotion.pkl","rb"))
-    results = pipe_lr.predict([docx])
-    return results[0]
 
-def get_prediction_proba(docx):
-    pipe_lr = joblib.load(open("/app/end2end-nlp-project/App/models/emotion.pkl","rb"))
-    results = pipe_lr.predict_proba([docx])
-    return results
-
-emotions_emoji_dict = {"anger":"😠", "fear":"😨😱", "joy":"🤧","love":"❤️", "sadness":"😔", "surprise":"😮"}
-
-ps = PorterStemmer()
-def transform_text(text):
-    text = text.lower()
-    text = nltk.word_tokenize(text)
-
-    y = []
-    for i in text:
-        if i.isalnum():
-            y.append(i)
-
-    text = y[:]
-    y.clear()
-
-    for i in text:
-        if i not in stopwords.words('english') and i not in string.punctuation:
-            y.append(i)
-
-    text = y[:]
-    y.clear()
-
-    for i in text:
-        y.append(ps.stem(i))
-
-    return " ".join(y)
 
 def main():
     st.title("Text Classifier App")
-    menu = ["Emotion Dection", "Email/SMS Spam Detection", "About"]
+    menu = ["Emotion Dection", "Email/SMS Spam Detection","Movies Recommendation","About"]
     choice = st.sidebar.selectbox("Menu",menu)
-
+#-----------------------------------------------------------------------------------------------------------------------------------
     if choice =="Emotion Dection":
+        def predict_emotions(docx):
+            pipe_lr = joblib.load(open("/app/end2end-nlp-project/App/models/emotion.pkl", "rb"))
+            results = pipe_lr.predict([docx])
+            return results[0]
+
+        def get_prediction_proba(docx):
+            pipe_lr = joblib.load(open("/app/end2end-nlp-project/App/models/emotion.pkl", "rb"))
+            results = pipe_lr.predict_proba([docx])
+            return results
+
+        emotions_emoji_dict = {"anger": "😠", "fear": "😨😱", "joy": "🤧", "love": "❤️", "sadness": "😔", "surprise": "😮"}
+
         pipe_lr = joblib.load(open("/app/end2end-nlp-project/App/models/emotion.pkl","rb"))
         st.subheader("Emotion In Text")
 
@@ -104,15 +76,36 @@ def main():
 
                 fig = alt.Chart(proba_df_clean).mark_bar().encode(x='emotions', y='probability', color='emotions')
                 st.altair_chart(fig, use_container_width=True)
-
+# -----------------------------------------------------------------------------------------------------------------------------------
     elif choice =="Email/SMS Spam Detection":
         st.subheader("Email/SMS Spam ")
         tfidf = pickle.load(open('/app/end2end-nlp-project/App/models/vectorizer.pkl', 'rb'))
         model = pickle.load(open('/app/end2end-nlp-project/App/models/model.pkl', 'rb'))
+        ps = PorterStemmer()
 
+        def transform_text(text):
+            text = text.lower()
+            text = nltk.word_tokenize(text)
 
+            y = []
+            for i in text:
+                if i.isalnum():
+                    y.append(i)
 
-        
+            text = y[:]
+            y.clear()
+
+            for i in text:
+                if i not in stopwords.words('english') and i not in string.punctuation:
+                    y.append(i)
+
+            text = y[:]
+            y.clear()
+
+            for i in text:
+                y.append(ps.stem(i))
+
+            return " ".join(y)
 
         input_sms = st.text_area("Enter the message")
 
@@ -129,6 +122,10 @@ def main():
                 st.header("Spam")
             else:
                 st.header("Not Spam")
+# -----------------------------------------------------------------------------------------------------------------------------------
+    
+    elif choice =="Movies Recommendation":
+        st.subheader("Movies Recommendation")
 
 
 
