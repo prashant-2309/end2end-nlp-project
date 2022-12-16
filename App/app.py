5491b3c2c9e17553a445504bb7c0ba4b18d5e6a8
prashant-2309 <singh2309prashant@gmail.com>
2022-12-16 09:49:50 +0530

    movie_recommendation

--- App/app.py
+++ App/app.py
@@ -6,6 +6,7 @@ import pickle
 import string
 from nltk.corpus import stopwords
 import nltk
+import requests
 from nltk.stem.porter import PorterStemmer
 nltk.download('punkt')
 nltk.download('stopwords')
@@ -123,14 +124,68 @@ def main():
             else:
                 st.header("Not Spam")
 # -----------------------------------------------------------------------------------------------------------------------------------
-    
+
     elif choice =="Movies Recommendation":
         st.subheader("Movies Recommendation")
 
+        def fetch_poster(movie_id):
+            url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(
+                movie_id)
+            data = requests.get(url)
+            data = data.json()
+            poster_path = data['poster_path']
+            full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+            return full_path
+
+        def recommend(movie):
+            index = movies[movies['title'] == movie].index[0]
+            distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+            recommended_movie_names = []
+            recommended_movie_posters = []
+            for i in distances[1:6]:
+                # fetch the movie poster
+                movie_id = movies.iloc[i[0]].movie_id
+                recommended_movie_posters.append(fetch_poster(movie_id))
+                recommended_movie_names.append(movies.iloc[i[0]].title)
+
+            return recommended_movie_names, recommended_movie_posters
+
+
+        movies = pickle.load(open('D:\ML\end2end-nlp-project\App\models\\movie_list.pkl', 'rb'))
+        similarity = pickle.load(open('D:\ML\end2end-nlp-project\App\models\\similarity.pkl', 'rb'))
+
+        movie_list = movies['title'].values
+        selected_movie = st.selectbox(
+            "Type or select a movie from the dropdown",
+            movie_list
+        )
+
+        if st.button('Show Recommendation'):
+            recommended_movie_names, recommended_movie_posters = recommend(selected_movie)
+            col1, col2, col3, col4, col5 = st.columns(5)
+            with col1:
+                st.text(recommended_movie_names[0])
+                st.image(recommended_movie_posters[0])
+            with col2:
+                st.text(recommended_movie_names[1])
+                st.image(recommended_movie_posters[1])
+
+            with col3:
+                st.text(recommended_movie_names[2])
+                st.image(recommended_movie_posters[2])
+            with col4:
+                st.text(recommended_movie_names[3])
+                st.image(recommended_movie_posters[3])
+            with col5:
+                st.text(recommended_movie_names[4])
+                st.image(recommended_movie_posters[4])
+
+        st.write("Due to the available dataset was till 2018, all the recommendation will be till 2018🤧")
 
 
 
 
+# -----------------------------------------------------------------------------------------------------------------------------------
     else:
         st.subheader("About")
         st.subheader("Made by Prashant Singh")
